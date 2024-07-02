24ecf81f2a7d99393a91848e989c3f775259927f
Narendra Ram <narenramm@gmail.com>
2024-07-02 19:18:12 +0530

    Remove nested .git directories to fix unclickable folder issue

--- install-all.bat
+++ install-all.bat
@@ -1,22 +1,59 @@
 @echo off
+setlocal enabledelayedexpansion
 
 :: Function to clone and install a repository
 :CloneAndInstall
-if not exist %1 (
-    echo Cloning %1...
-    git clone %2 %1
+set "repo_name=%~1"
+set "repo_url=%~2"
+
+echo Debug: repo_name is "!repo_name!"
+echo Debug: repo_url is "!repo_url!"
+
+if exist "!repo_name!" (
+    echo Folder !repo_name! already exists.
+    if exist "!repo_name!\.git" (
+        echo It appears to be a git repository. Updating...
+        pushd "!repo_name!"
+        git pull
+        popd
+    ) else (
+        echo It's an empty folder. Removing and cloning fresh...
+        rmdir /s /q "!repo_name!"
+        git clone "!repo_url!" "!repo_name!"
+    )
+) else (
+    echo Cloning !repo_name! from !repo_url!...
+    git clone "!repo_url!" "!repo_name!"
 )
-echo Installing dependencies for %1...
-cd %1
+
+if errorlevel 1 (
+    echo Failed to clone or update !repo_name!. Exiting...
+    exit /b 1
+)
+
+echo Installing dependencies for !repo_name!...
+pushd "!repo_name!"
 npm install
-cd ..
-goto :eof
+if errorlevel 1 (
+    echo Failed to install dependencies for !repo_name!. Exiting...
+    popd
+    exit /b 1
+)
+popd
+exit /b 0
 
 :: Clone and install steadfast-stoic-app
-call :CloneAndInstall steadfast-stoic-app https://github.com/narenkram/steadfast-stoic-app
+set "app_name=steadfast-stoic-app"
+set "app_url=https://github.com/narenkram/steadfast-stoic-app.git"
+call :CloneAndInstall "!app_name!" "!app_url!"
+if errorlevel 1 exit /b
 
 :: Clone and install steadfast-stoic-api
-call :CloneAndInstall steadfast-stoic-api https://github.com/narenkram/steadfast-stoic-api
+set "api_name=steadfast-stoic-api"
+set "api_url=https://github.com/narenkram/steadfast-stoic-api.git"
+call :CloneAndInstall "!api_name!" "!api_url!"
+if errorlevel 1 exit /b
 
 echo Installation complete.
 pause
+endlocal
