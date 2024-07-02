a68c07bbc53d73d77ba1c7d19b48933a28a030f1
Narendra Ram <narenramm@gmail.com>
2024-07-02 21:29:50 +0530

    Update update-all.bat

--- update-all.bat
+++ update-all.bat
@@ -1,13 +1,32 @@
 @echo off
+setlocal enabledelayedexpansion
+
+REM Update steadfast-stoic-app
 echo Updating steadfast-stoic-app...
 cd steadfast-stoic-app
-git pull https://github.com/narenkram/steadfast-stoic-app
+git pull https://github.com/narenkram/steadfast-stoic-app main
+if !errorlevel! neq 0 (
+    echo Error updating steadfast-stoic-app.
+    goto :error
+)
 cd ..
 
+REM Update steadfast-stoic-api
 echo Updating steadfast-stoic-api...
 cd steadfast-stoic-api
-git pull https://github.com/narenkram/steadfast-stoic-api
+git pull https://github.com/narenkram/steadfast-stoic-api main
+if !errorlevel! neq 0 (
+    echo Error updating steadfast-stoic-api.
+    goto :error
+)
 cd ..
 
 echo Update complete.
+goto :end
+
+:error
+echo An error occurred during the update process.
+
+:end
 pause
+endlocal
