2743d06750c86d2ed4510efda26515f22c715205
Narendra Ram <narenramm@gmail.com>
2024-07-02 18:03:19 +0530

    Revert "Update update-all.bat"
    This reverts commit 610cc41cff39e8e7b8415c3e89fe3f1c92ca9e62.

--- update-all.bat
+++ update-all.bat
@@ -1,22 +1,13 @@
 @echo off
-:: Function to update or clone a repository
-:UpdateOrClone
-if exist %1 (
-    echo Updating %1...
-    cd %1
-    git pull %2
-    cd ..
-) else (
-    echo Cloning %1...
-    git clone %2 %1
-)
-goto :eof
+echo Updating steadfast-stoic-app...
+cd steadfast-stoic-app
+git pull https://github.com/narenkram/steadfast-stoic-app
+cd ..
 
-:: Update or clone steadfast-stoic-app
-call :UpdateOrClone steadfast-stoic-app https://github.com/narenkram/steadfast-stoic-app
-
-:: Update or clone steadfast-stoic-api
-call :UpdateOrClone steadfast-stoic-api https://github.com/narenkram/steadfast-stoic-api
+echo Updating steadfast-stoic-api...
+cd steadfast-stoic-api
+git pull https://github.com/narenkram/steadfast-stoic-api
+cd ..
 
 echo Update complete.
 pause
