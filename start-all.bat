2ce95167e07a3dc700f115985695bc096264b0d6
Narendra Ram <narenramm@gmail.com>
2024-07-04 14:03:29 +0530

    add automation to open socket connection

--- start-all.bat
+++ start-all.bat
@@ -42,13 +42,19 @@ start /min cmd /c "cd steadfast-stoic-app && npm run dev"
 REM Start the API in a new command prompt window
 start /min cmd /c "cd steadfast-stoic-api && node server.js"
 
+REM Start the Flattrade websocket in a new command prompt window
+start /min cmd /c "cd steadfast-websocket\flattrade && python flattrade-websocket.py"
+
 REM Wait for a few seconds to allow the app to start
 timeout /t 5
 
 REM Open the default browser to the app's URL
 start http://localhost:5173
 
-echo Services started and browser opened. Close this window to stop all services.
+REM Open the default browser to the API's URL
+start http://localhost:3000
+
+echo Services started and browsers opened. Close this window to stop all services.
 echo Press any key to stop all services...
 pause > nul
 
