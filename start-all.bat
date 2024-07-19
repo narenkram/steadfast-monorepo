4b22cbee59d995e2cb4b98798d014991c0477410
Narendra Ram <narenramm@gmail.com>
2024-07-19 23:24:15 +0530

    update bat script

--- start-all.bat
+++ start-all.bat
@@ -5,10 +5,12 @@ setlocal enabledelayedexpansion
 echo Choose an option:
 echo 1. Update the app, api and websocket (Recommended once a day)
 echo 2. Run existing version
-set /p choice="Enter your choice (1 or 2): "
+echo 3. Exit
+set /p choice="Enter your choice (1, 2, or 3): "
 
 if "%choice%"=="1" goto update
 if "%choice%"=="2" goto run
+if "%choice%"=="3" goto end
 echo Invalid choice. Please try again.
 goto menu
 
@@ -44,24 +46,30 @@ if !errorlevel! neq 0 (
 cd ..
 
 echo Update complete.
+goto menu
 
 :run
 REM Start the API in a new command prompt window
+echo Starting API...
 start /min cmd /c "cd steadfast-stoic-api && node server.js"
 
 REM Start the app in a new command prompt window
+echo Starting app...
 start /min cmd /c "cd steadfast-stoic-app && npm run dev"
 
 REM Start the Flattrade websocket in a new command prompt window
+echo Starting Flattrade websocket...
 start /min cmd /c "cd steadfast-websocket\flattrade && python flattrade-websocket.py"
 
 REM Wait for a few seconds to allow the app to start
 timeout /t 5
 
 REM Open the default browser to the API's URL
+echo Opening browser to API's URL...
 start http://localhost:3000
 
 REM Open the default browser to the app's URL
+echo Opening browser to app's URL...
 start http://localhost:5173
 
 echo Services started and browsers opened. Close this window to stop all services.
@@ -75,12 +83,7 @@ REM Kill all python processes
 taskkill /F /IM python.exe > nul 2>&1
 
 echo All services stopped.
-goto :end
-
-:error
-echo An error occurred during the update process.
-pause
-exit /b 1
+goto menu
 
 :end
 endlocal
