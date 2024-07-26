397ca952d0f9a60cf54d3ec06e8b1f5a2b15d593
Krishna Singh Shahi <63533035+krishnasinghshahi@users.noreply.github.com>
2024-07-26 22:53:45 +0530

    Removed api_helper related command and added for dhanhq

--- linux-install-all.sh
+++ linux-install-all.sh
@@ -47,18 +47,21 @@ cd steadfast-websocket
 echo "Installing WebSocket dependencies..."
 echo "Installing Flattrade dependencies..."
 cd flattrade
-chmod +x api_helper.py
 chmod +x flattrade-websocket.py
 pip install -r requirements.txt || error
 cd ..
 
 echo "Installing Shoonya dependencies..."
 cd shoonya
-chmod +x api_helper.py
 chmod +x shoonya-websocket.py
 pip install -r requirements.txt || error
-cd ../..
+cd ..
 
+echo "Installing Dhanhq dependencies..."
+cd dhanhq
+chmod +x dhan-websocket.py
+pip install -r requirements.txt || error
+cd ../.. 
 echo "Repositories cloned and dependencies installed successfully."
 
 # End of script
