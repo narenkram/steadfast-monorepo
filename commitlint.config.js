869619e327ba01d52ea197f749e1973e96dba9c8
Narendra Ram <narenramm@gmail.com>
2024-10-08 00:08:54 +0530

    chore: add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore"],
+    ],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-max-length": [2, "always", 50],
+  },
+};
